e1fbf41ebb7e987630a023779d00067fb7dd6196
Cody Alejo <cody.alejo1997@gmail.com>
2023-02-22 20:19:17 -0600

    finished exercise JS 101 Explore Arrays

---
+++ javascript-exercises/js101ExploreArray.js
@@ -0,0 +1,19 @@
+const digitalCraftsCrew = [
+    "Jamie",
+    "Dr. Vicki",
+    "Jada",
+    "Athena",
+    "Dee",
+    "Ernesto",
+    "Robbie"
+];
+console.log(digitalCraftsCrew.length) ;
+console.log(digitalCraftsCrew.push("Cody"));
+console.log(digitalCraftsCrew.pop());
+console.log(digitalCraftsCrew.indexOf('Dr. Vicki'));
+
+
+//Length of the array
+//Add a new item to the END of the array
+//Remove item from the END of the array
+//Get the index of "Dr. Vicki"
